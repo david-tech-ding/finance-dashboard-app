37b5204187303ebaea6f45ff18131be2396be811
David Ding <dingtechxing16@gmail.com>
2023-09-13 16:20:45 +0800

    MUI Configurations

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { createTheme } from "@mui/material/styles";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>Dashboard Page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>Predictions Page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
