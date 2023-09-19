f09c7f19dc344272b69e0038e0459b798915e94f
David Ding <dingtechxing16@gmail.com>
2023-09-19 18:58:10 +0100

    MongoDB set up

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 import "@/index.css";
@@ -11,6 +10,7 @@ export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
   middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+
 setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
