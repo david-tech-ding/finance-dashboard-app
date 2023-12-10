f3f120dbfaf431567afaf6bcb326024b5df0c847
David Ding <dingtechxing16@gmail.com>
2023-12-10 19:43:04 +0000

    Inspect variables

--- src/expanded-theme.ts
+++ src/expanded-theme.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
     [key: number]: string;
