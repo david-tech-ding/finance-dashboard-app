7d5b044595f2188de06e78f9428d26a98305df77
David Ding <dingtechxing16@gmail.com>
2023-12-10 19:33:23 +0000

    Deployment variables with Fly.io

--- server/index.js
+++ server/index.js
@@ -31,9 +31,9 @@ app.use("/product", productRoutes);
 app.use("/transaction", transactionRoutes);
 
 /* Mongoose Setup */
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
